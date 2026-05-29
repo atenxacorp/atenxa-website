855d9b700519b8eb21c6a50e6ce054e84cc4575f
atenxacorp <atenxacorp@gmail.com>
2026-05-29 15:32:04 +0800

    Update header.php

--- api/header.php
+++ api/header.php
@@ -727,7 +727,7 @@ footer {
 <!-- NAV -->
 <nav>
   <a href="/" class="logo">
-  <img src="https://i.imgur.com/Yyo0Zul.png" alt="Atenxa" />
+  <img src="/propic.png" alt="Atenxa" />
   </a>
   <div class="nav-right">
     <a href="/#technology" class="nav-link">Technology</a>
